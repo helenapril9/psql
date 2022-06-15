ca923da37e5e012c255e12cb7bf5201c54494b1a
Nelly Porvatova <dolgikhhel@yandex.ru>
2022-06-15 23:01:04 +0400

    Last commit1

--- Script-3.sql
+++ Script-3.sql
@@ -1,27 +1,26 @@
 CREATE TABLE IF NOT EXISTS Jenre (
- 	id SERIAL PRIMARY KEY,
- 	jenre_name VARCHAR(60) NOT null
+ 	  id SERIAL PRIMARY KEY,
+ 	  jenre_name VARCHAR(60) NOT null
  );
   CREATE TABLE IF NOT EXISTS Artist (
- 	id SERIAL PRIMARY KEY,
- 	artist_name VARCHAR(60) NOT null
+ 	  id SERIAL PRIMARY KEY,
+ 	  artist_name VARCHAR(60) NOT null
  );
   CREATE TABLE IF NOT EXISTS Album (
- 	id SERIAL PRIMARY KEY,
- 	jenre_name VARCHAR(60) NOT null
-  album_name VARCHAR(60) NOT null
-   album_year VARCHAR(4) NOT null
+ 	  id SERIAL PRIMARY KEY,
+    album_name VARCHAR(60) NOT null
+    album_year VARCHAR(4) NOT null
  );
   CREATE TABLE IF NOT EXISTS Collection (
- 	id SERIAL PRIMARY KEY,
- 	jenre_name VARCHAR(60) NOT null
-   collection_year VARCHAR(4) NOT null
+ 	  id SERIAL PRIMARY KEY,
+ 	  collection_name VARCHAR(60) NOT null
+    collection_year VARCHAR(4) NOT null
  );
  CREATE TABLE IF NOT EXISTS Track (
- 	id SERIAL PRIMARY KEY,
- 	track_name VARCHAR(60) NOT NULL,
- 	diration NUMERIC(3,2) NOT NULL,
- 	album_id INTEGER NOT NULL REFERENCES Album(id)
+ 	  id SERIAL PRIMARY KEY,
+ 	  track_name VARCHAR(60) NOT NULL,
+ 	  diration NUMERIC(3,2) NOT NULL,
+ 	  album_id INTEGER NOT NULL REFERENCES Album(id)
  );
   CREATE TABLE IF NOT EXISTS Artist_jenre (
     jenre_id INTEGER NOT NULL REFERENCES Jenre(id),
