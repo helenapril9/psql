bb36411c9262d498744d31f3b823b99a4fad29fa
Nelly Porvatova <dolgikhhel@yandex.ru>
2022-06-16 21:50:00 +0400

    Initial commit

--- Script-3.sql
+++ Script-3.sql
@@ -7,26 +7,30 @@ CREATE TABLE IF NOT EXISTS Jenre (
  	  artist_name VARCHAR(60) NOT null
  );
   CREATE TABLE IF NOT EXISTS Album (
- 	  id SERIAL PRIMARY KEY,
-    album_name VARCHAR(60) NOT null
-    album_year VARCHAR(4) NOT null
- );
+ 	 id SERIAL PRIMARY KEY,
+    album_name VARCHAR(60) NOT null,
+    album_year INTEGER NOT null
+   );
+
   CREATE TABLE IF NOT EXISTS Collection (
  	  id SERIAL PRIMARY KEY,
- 	  collection_name VARCHAR(60) NOT null
-    collection_year VARCHAR(4) NOT null
+ 	  collection_name VARCHAR(60) NOT null,
+      collection_year INTEGER NOT null
  );
+
  CREATE TABLE IF NOT EXISTS Track (
  	  id SERIAL PRIMARY KEY,
  	  track_name VARCHAR(60) NOT NULL,
  	  diration NUMERIC(3,2) NOT NULL,
  	  album_id INTEGER NOT NULL REFERENCES Album(id)
  );
+
   CREATE TABLE IF NOT EXISTS Artist_jenre (
     jenre_id INTEGER NOT NULL REFERENCES Jenre(id),
     artist_id INTEGER NOT NULL REFERENCES Artist(id),
     CONSTRAINT pk PRIMARY KEY (jenre_id, artist_id)
  );
+
 CREATE TABLE IF NOT EXISTS Album_artist (
     album_id INTEGER NOT NULL REFERENCES Album(id),
     artist_id INTEGER NOT NULL REFERENCES Artist(id),
@@ -38,8 +42,3 @@ CREATE TABLE IF NOT EXISTS Collection_track (
     collection_id INTEGER NOT NULL REFERENCES Collection(id),
     CONSTRAINT pk2 PRIMARY KEY (track_id, collection_id)
  );
-
-
-
-	
-
